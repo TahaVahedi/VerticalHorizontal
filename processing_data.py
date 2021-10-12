1807d76576efd93359232f3ee7ce02608e6cd6e9
taha vahedi <67023281+TahaVahedi@users.noreply.github.com>
2021-10-12 20:52:42 +0330

    Update and rename processing_data(vertical-horizontal).py to processing_data.py

--- processing_data.py
+++ processing_data.py
@@ -1,12 +1,12 @@
-# images verticaler rotate 90 degrees
+# preprocessing and create vertical set images (rotate 90 degrees)
 import os
 from glob import glob as g 
 import cv2
 
 
 # train test split
-
-original_dataset = "./landspace_dataset_original/"
+#download your images and put them in same folder and update the original_dataset 
+original_dataset = "./landspace_dataset_original/"  # here i use https://www.kaggle.com/arnaud58/landscape-pictures
 all_imges = g(original_dataset + "*.jpg")
 n_images = int(len(all_imges))
 print(n_images)
@@ -46,16 +46,18 @@ def saveIMG(image, label, name, train="True"):
 train_path = "./dataset/train/"
 all_train_images = g(str(train_path)+"*.jpg")
 
-
+print("start processing...")
 
 for src in all_train_images:
-    name = str(src.split("\\")[-1])
+    name1 = str(src.split("\\")[-1])
+    name = train_path+name1
 
-    img1 = cv2.resize(cv2.imread(train_path+name ,cv2.IMREAD_COLOR), dim, interpolation=cv2.INTER_AREA)
-    saveIMG(img1, "horizontal", name, train="True")
+    img1 = cv2.resize(cv2.imread(name ,cv2.IMREAD_COLOR), dim, interpolation=cv2.INTER_AREA)
+    saveIMG(img1, "horizontal", name1, train="True")
     
     img2 = cv2.rotate(img1, cv2.cv2.ROTATE_90_CLOCKWISE) 
-    saveIMG(img2, "vertical", name, train="True")
+    saveIMG(img2, "vertical", name1, train="True")
+    os.remove(name)
     
 
 print("train set Done!")   
@@ -69,13 +71,15 @@ all_test_images = g(str(test_path)+"*.jpg")
 
 
 for src in all_test_images:
-    name = str(src.split("\\")[-1])
+    name1 = str(src.split("\\")[-1])
+    name = train_path+name1
 
-    img1 = cv2.resize(cv2.imread(test_path+name ,cv2.IMREAD_COLOR), dim, interpolation=cv2.INTER_AREA)
-    saveIMG(img1, "horizontal", name, train="null")
+    img1 = cv2.resize(cv2.imread(name ,cv2.IMREAD_COLOR), dim, interpolation=cv2.INTER_AREA)
+    saveIMG(img1, "horizontal", name1, train="null")
     
     img2 = cv2.rotate(img1, cv2.cv2.ROTATE_90_CLOCKWISE) 
-    saveIMG(img2, "vertical", name, train="null")
+    saveIMG(img2, "vertical", name1, train="null")
+    os.remove(name)
     
 
 print("test set Done!")   
